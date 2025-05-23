fb6fa9456ec50d64b4a8e457f56ec52302e82b0a
NSaldarriagaV <141058207+NSaldarriagaV@users.noreply.github.com>
2025-05-22 21:47:49 -0500

    cambio bd

--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Inicializa la configuración de entorno
+env = environ.Env(
+    DEBUG=(bool, False)  # valor por defecto si no se encuentra en el .env
+)
+
+# Ruta al archivo .env
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -81,12 +93,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
+
 
 
 # Password validation
